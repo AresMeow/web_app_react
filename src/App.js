5887a82c704c988a5a26c64448eae9ed29473580
unknown <aresxmeow@gmail.com>
2024-09-19 21:26:42 +1000

    init

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 
 const telegramApp = window.Telegram.WebApp;
@@ -10,8 +11,13 @@ function App() {
   const onClose = () => {
     telegramApp.close();
   };
-  
-  return <div className="App">work</div>;
+
+  return (
+    <div className="App">
+      work
+      <button onClick={onClose}>close</button>
+    </div>
+  );
 }
 
 export default App;
