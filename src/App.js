4cfea9b2bc1ca5ca661e75088f10eb5b46118dd4
unknown <aresxmeow@gmail.com>
2024-09-29 10:14:15 +1000

    init

--- src/App.js
+++ src/App.js
@@ -5,16 +5,35 @@ const telegramApp = window.Telegram.WebApp;
 
 function App() {
     return (
-        <svg
-            className="background-svg"
-            width="390"
-            height="722"
-            viewBox="0 0 390 722"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-        >
-            <path d="M0 0H390V722H0V0Z" fill="#13121a" />
-        </svg>
+        <div className="App">
+            <svg
+                className="background-svg"
+                width="390"
+                height="722"
+                viewBox="0 0 390 722"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <path d="M0 0H390V722H0V0Z" fill="currentColor" />
+            </svg>
+
+            <svg
+                className="overlay-svg"
+                width="390"
+                height="70"
+                viewBox="0 0 390 70"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M0 0L22 12C43 23 87 47 130 47C173 47 217 23 260 19C303 15 347 31 368 39L390 47V70H368C347 70 303 70 260 70C217 70 173 70 130 70C87 70 43 70 22 70H0V0Z"
+                    fill="currentColor"
+                    fillOpacity="0.6"
+                />
+            </svg>
+        </div>
     );
 }
 
